8ccdc8320ef3d5e69aa7f59dc833c119e8374eab
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-11 12:12:57 +0200

    "Updated App.css with new styles for .cardContainer and modified CardContainer.jsx to add 'cardContainer' class to its div element."

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,7 +1,7 @@
 import PlayerCard from "./PlayerCard";
 const CardContainer = ({ data }) => {
   return (
-    <div>
+    <div className="cardContainer">
       {data.map((player, index) => (
         <PlayerCard player={player} key={index} />
       ))}
