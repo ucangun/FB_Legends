01668fbf81c2d3f90330d88897b921de093b5a0b
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-11 15:32:01 +0200

    "Updated App.css with new styles, modified PlayerCard.jsx to add conditional rendering and hover effect, and updated data.js with new player images and statistics."

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 
 const PlayerCard = ({ player }) => {
-  const [selected, setSelected] = useState(null);
-  const { name, img, statistics } = player;
+  const [selectedId, setSelectedId] = useState(null);
+  const { id, name, img, statistics } = player;
 
-  const handleClick = () => {
-    setSelected(name !== selected ? name : null);
+  const handleClick = (id) => {
+    setSelectedId(id !== selectedId ? id : null);
   };
 
   return (
     <div className="playerCard">
-      <div className="cardTop">
-        <img onClick={handleClick} src={img} alt="" />
+      <div className="playerCard-top">
+        {id === selectedId ? (
+          <div onClick={() => handleClick(id)} className="playerStats">
+            {statistics.map((item, i) => (
+              <p key={i}>{item}</p>
+            ))}
+          </div>
+        ) : (
+          <img onClick={() => handleClick(id)} src={img} alt="" />
+        )}
       </div>
       <h3>{name}</h3>
-      <div className="playerStats">{statistics}</div>
     </div>
   );
 };
--- src/helper/data.js
+++ src/helper/data.js
@@ -2,7 +2,7 @@ export const data = [
   {
     id: 1,
     name: "Volkan Demirel",
-    img: "https://s22928.pcdn.co/wp-content/uploads/2018/03/Kareem-Abdul-Jabbar.jpg",
+    img: "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTqFQ63Qd16jBOpRYvez50IjJXqSass7ESuCdp19s22mWN_DOEysur4bInincZB8Pmmz8l_BuWR2Kb61tw",
     statistics: [
       "38,387 points",
       "17,440 rebounds",
@@ -12,8 +12,8 @@ export const data = [
   },
   {
     id: 2,
-    name: "Rüstü Recber",
-    img: "https://cdn.vox-cdn.com/thumbor/8yWTZWnt03DGQkU9ZVdGluZWqRM=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23373244/147497571.jpg",
+    name: "Aykut Kocaman",
+    img: "https://i.pinimg.com/564x/f6/dd/4d/f6dd4d49e6cea88b8de5abf408a9cf71.jpg",
     statistics: [
       "21,791 points",
       "8,974 rebounds",
@@ -24,7 +24,7 @@ export const data = [
   {
     id: 3,
     name: "Gökhan Gönül",
-    img: "https://pbs.twimg.com/media/EQwzU_RXUAAAATG?format=jpg&name=4096x4096",
+    img: "https://www.cumhuriyet.com.tr/Archive/2021/7/2/1849091/kapak_072926.jpg",
     statistics: [
       "31,419 points",
       "23,924 rebounds",
@@ -35,7 +35,7 @@ export const data = [
   {
     id: 4,
     name: "Alex de Souza",
-    img: "https://cdn.britannica.com/27/189527-050-5BAD12C2/Magic-Johnson.jpg",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/40/Alexsandro_de_Souza.jpg",
     statistics: [
       "17,707 points",
       "6,559 rebounds",
@@ -46,7 +46,7 @@ export const data = [
   {
     id: 5,
     name: "Oguz Cetin",
-    img: "https://cdn.europosters.eu/image/1300/posters/michael-jordan-i116434.jpg",
+    img: "https://s1.dmcdn.net/v/SEYPY1VYJxsphNVKn/x1080",
     statistics: [
       "32,292 points",
       "6,672 rebounds",
@@ -57,7 +57,7 @@ export const data = [
   {
     id: 6,
     name: "Semih Şentürk",
-    img: "https://i.pinimg.com/564x/49/76/10/497610981fdbe0158ba09b5c8418d87d--nba-players-basketball-players.jpg",
+    img: "https://image.hurimg.com/i/hurriyet/75/750x422/5b954fd118c773043c6a3814.jpg",
     statistics: [
       "36,928 points",
       "14,968 rebounds",
@@ -68,7 +68,7 @@ export const data = [
   {
     id: 7,
     name: "Mehmet Aurélio",
-    img: "https://cdn.artphotolimited.com/images/60913d60bd40b85323893a87/1000x1000/hakeem-olajuwon-von-den-houston-rockets.jpg",
+    img: "https://tmssl.akamaized.net//images/foto/galerie/mehmet-aurelio-fenerbahce-1590407019-39481.jpg",
     statistics: [
       "26,946 points",
       "13,748 rebounds",
@@ -78,8 +78,8 @@ export const data = [
   },
   {
     id: 8,
-    name: "Mehmet Topuz",
-    img: "https://staticg.sportskeeda.com/editor/2018/10/6bb16-15398694878129-800.jpg",
+    name: "Lefter Küçükandonyadis",
+    img: "https://cdn.ntvspor.net/dbb4275c659c4bbf82b3cb0fcf2f4039.jpg?mode=crop&w=940&h=626",
     statistics: [
       "28,596 points",
       "13,099 rebounds",
@@ -90,7 +90,7 @@ export const data = [
   {
     id: 9,
     name: "İlker Yağcıoğlu",
-    img: "https://basketballforever.com/wp-content/uploads/2020/04/Scottie-Pippen-Chicago-Bulls.jpg",
+    img: "https://pbs.twimg.com/media/FCOsYXEVgAQaGWv.jpg:large",
     statistics: [
       "18,940 points",
       "7,494 rebounds",
@@ -101,7 +101,7 @@ export const data = [
   {
     id: 10,
     name: "Tuncay Şanlı",
-    img: "https://cdn.britannica.com/37/231237-050-0CB78D66/Patrick-Ewing-New-York-Knicks-1986.jpg",
+    img: "https://www.cumhuriyet.com.tr/Archive/2023/5/13/2080995/kapak_165540.jpg",
     statistics: [
       "24,815 points",
       "11,607 rebounds",
@@ -112,7 +112,7 @@ export const data = [
   {
     id: 11,
     name: "İsmail Kartal",
-    img: "https://upload.wikimedia.org/wikipedia/commons/9/96/Kobe_Bryant_8.jpg",
+    img: "https://s2.dmcdn.net/v/V30P71ad8colHVyAg/x1080",
     statistics: [
       "33,643 points",
       "7,047 rebounds",
@@ -122,8 +122,8 @@ export const data = [
   },
   {
     id: 12,
-    name: "Cristian",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    name: "Cristian Baroni",
+    img: "https://m.superfb.com/img/2019/11/16/baroniden-kocaman-yanal-kiyasi-ve-aziz-sfb-642-800.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -134,7 +134,7 @@ export const data = [
   {
     id: 13,
     name: "Ferdi Kadıoğlu",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://img.piri.net/mnresize/712/-/piri/upload/3/2024/3/5/b93a218e-3ejmtxzb26va0ayngy0gc.png",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -145,7 +145,7 @@ export const data = [
   {
     id: 14,
     name: "Diego Lugano",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://i.redd.it/x6p25l6esg2c1.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -156,7 +156,7 @@ export const data = [
   {
     id: 15,
     name: "Moussa Sow",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://i.haberglobal.com.tr/storage/haber/2020/04/18/moussa-sow-fenerbahce-tercihimden-dolayi-gercekten-cok-mutluyum_1587200377.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -167,7 +167,7 @@ export const data = [
   {
     id: 16,
     name: "Elvir Bolic",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://i.pinimg.com/564x/4a/02/aa/4a02aa35af579b38c303230de864ad17.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -177,8 +177,8 @@ export const data = [
   },
   {
     id: 17,
-    name: "Deivid",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    name: "Deivid de Souza",
+    img: "https://yasamoykusu.com/_docs/story/Deivid_de_Souza0_yasamoykusu.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -189,7 +189,7 @@ export const data = [
   {
     id: 18,
     name: "Bright Osayi-Samuel",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://tmssl.akamaized.net/images/foto/galerie/bright-osayi-samuel-fenerbahce-2023-2024-1703080514-124594.jpg?lm=1703080535",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -200,7 +200,7 @@ export const data = [
   {
     id: 19,
     name: "Ümit Özat",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://i.pinimg.com/736x/02/a9/e8/02a9e8227a8db86bcebe74b2670aa26b.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -211,7 +211,7 @@ export const data = [
   {
     id: 20,
     name: "Bülent Uygun",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://volkanbk3.wordpress.com/wp-content/uploads/2008/09/liveimages5cyenifotoanaliz5ctesadfcfen20futbolcu20oldu5c07.jpg?w=584",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -222,7 +222,7 @@ export const data = [
   {
     id: 21,
     name: "Dirk Kuyt",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://media.fenerbahce.org/FB/media/FB/pic_lib/2013-04-04_kuyt538.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -233,7 +233,7 @@ export const data = [
   {
     id: 22,
     name: "İrfan Can Kahveci",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://ia.tmgrup.com.tr/14da24/1160/650/0/0/2048/1147?u=https://i.tmgrup.com.tr/rt/2024/03/05/irfan-can-kahveciden-mujdeli-haber-1709624130484.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -244,7 +244,7 @@ export const data = [
   {
     id: 23,
     name: "Rıdvan Dilmen",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://i.pinimg.com/originals/c4/90/b3/c490b37a3f226ae3b12dd5788605ddb9.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -255,7 +255,7 @@ export const data = [
   {
     id: 24,
     name: "Miroslav Stoch",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://trthaberstatic.cdn.wp.trt.com.tr/resimler/88000/89974.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -266,7 +266,7 @@ export const data = [
   {
     id: 25,
     name: "Martin Skrtel",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://tmssl.akamaized.net/images/foto/big/martin-skrtel-von-fenerbahce-istanbul-1549958201-20450.jpg?lm=1549958210",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -277,7 +277,7 @@ export const data = [
   {
     id: 26,
     name: "Fabio Luciano",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://media06.ligtv.com.tr/img/gallery/2019326_lucianonun_unutamadg_fenerbahce_11i/luciano.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -287,8 +287,8 @@ export const data = [
   },
   {
     id: 27,
-    name: "Joseph Yobo",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    name: "Bruno Alves",
+    img: "https://media.fenerbahce.org/FB/media/FB/pic_lib/balves538_36536_3594263.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -298,8 +298,8 @@ export const data = [
   },
   {
     id: 28,
-    name: "Mert Nobre",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    name: "Andre Santos",
+    img: "https://iaaspr.tmgrup.com.tr/b72ab5/613/641/0/0/0/0?u=http://i.aspor.com.tr/galeri/fenerbahce/andre-santos-fenerbahceye-geri-mi-donuyor/1_v2_lb.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -310,7 +310,7 @@ export const data = [
   {
     id: 29,
     name: "Toni Schumacher",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://i.pinimg.com/564x/e1/c0/77/e1c077dac61ba6921762f4a1dc2ec767.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -321,7 +321,7 @@ export const data = [
   {
     id: 30,
     name: "Raúl Meireles",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://image.hurimg.com/i/hurriyet/75/750x422/55ea6323f018fbb8f87ca2ca.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -332,7 +332,7 @@ export const data = [
   {
     id: 31,
     name: "Roberto Carlos",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://iaftm.tmgrup.com.tr/c980fc/1200/627/0/19/1200/647?u=https://iftm.tmgrup.com.tr/2022/02/01/roberto-carlos-sahalara-geri-donuyor-yeni-takimi-1643701366917.jpeg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -343,7 +343,7 @@ export const data = [
   {
     id: 32,
     name: "Pierre Webó",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://image.milimaj.com/i/milliyet/75/869x477/5c8e21a945d2a09e00a316f1.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -354,7 +354,7 @@ export const data = [
   {
     id: 33,
     name: "Robin van Persie",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    img: "https://i.pinimg.com/736x/ad/61/6f/ad616ff979f67eea60495e73293f9876.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -362,10 +362,11 @@ export const data = [
       "11 All Star",
     ],
   },
+
   {
     id: 34,
-    name: "Haim Revivo",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    name: "Pierre van Hooijdonk",
+    img: "https://pbs.twimg.com/media/FP_bE3FWUAYU6_k.jpg:large",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -375,8 +376,8 @@ export const data = [
   },
   {
     id: 35,
-    name: "Aykut Kocaman",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    name: "Mateja Kežman",
+    img: "https://image.cnnturk.com/i/cnnturk/75/740x416/527e3038992df107301d4cb6.jpg",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
@@ -386,8 +387,52 @@ export const data = [
   },
   {
     id: 36,
-    name: "Pierre van Hooijdonk",
-    img: "https://freshairarchive.org/sites/default/files/styles/square_800/public/2021-08/CharlesBarkley.jpg?itok=lkOGUQ2t",
+    name: "Edin Dzeko",
+    img: "https://cdn.ntvspor.net/ebfa28f15fe34dd5bf684146966b1eba.jpg?mode=crop&w=940&h=626",
+    statistics: [
+      "23,757 points",
+      "12,546 rebounds",
+      "4,215 assists",
+      "11 All Star",
+    ],
+  },
+  {
+    id: 37,
+    name: "Can Bartu",
+    img: "https://www.gazetekadikoy.com.tr/Uploads/gazetekadikoy.com.tr/haberler/200495576260797-img.jpg",
+    statistics: [
+      "23,757 points",
+      "12,546 rebounds",
+      "4,215 assists",
+      "11 All Star",
+    ],
+  },
+  {
+    id: 38,
+    name: "Stephen Appiah",
+    img: "https://pbs.twimg.com/media/EVGVMQXUUAAMjga?format=jpg&name=4096x4096",
+    statistics: [
+      "23,757 points",
+      "12,546 rebounds",
+      "4,215 assists",
+      "11 All Star",
+    ],
+  },
+  {
+    id: 39,
+    name: "Nicolas Anelka",
+    img: "https://cdn1.ntv.com.tr/gorsel/sMtybA5AgUO1hYgEhweYGg.jpg?width=1000&mode=crop&scale=both",
+    statistics: [
+      "23,757 points",
+      "12,546 rebounds",
+      "4,215 assists",
+      "11 All Star",
+    ],
+  },
+  {
+    id: 39,
+    name: "Dusan Tadic",
+    img: "https://tmssl.akamaized.net/images/foto/galerie/dusan-tadic-fenernahce-2023-24-1689579033-111706.jpg?lm=1689579051",
     statistics: [
       "23,757 points",
       "12,546 rebounds",
