606ef2ca9a62f8969850e9d0a973a0b6171e8004
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-11 12:34:51 +0200

    "Added id property to each player object in data.js and implemented selected state and click handler in PlayerCard.jsx"

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,10 +1,20 @@
+import React, { useState } from "react";
+
 const PlayerCard = ({ player }) => {
+  const [selected, setSelected] = useState(null);
   const { name, img, statistics } = player;
+
+  const handleClick = () => {
+    setSelected(name !== selected ? name : null);
+  };
 
   return (
     <div className="playerCard">
-      <img src={img} alt="" />
+      <div className="cardTop">
+        <img onClick={handleClick} src={img} alt="" />
+      </div>
       <h3>{name}</h3>
+      <div className="playerStats">{statistics}</div>
     </div>
   );
 };
